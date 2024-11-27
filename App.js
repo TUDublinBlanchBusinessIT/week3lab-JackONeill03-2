7f966d3440613613f1e64c2ca6e3edf8b2e283b5
JackONeill03 <B00155573@mytudublin.ie>
2024-11-27 10:22:32 +0000

    Added Parts

--- App.js
+++ App.js
@@ -3,11 +3,20 @@ import React, {useState} from 'react';
 
 
 export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
+const [fname, setFname] = useState("Joe");
+const [lname, setLname] = useState("Bloggs");
+const [dob, setDob] = useState("22 August 1990");
+function buttonClicked() {
+  Alert.alert("button clicked");
+}
+<Button title="SUBMIT" onPress={buttonClicked}/>
   return(
     <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
+<TextInput placeholder="Enter your firstname" onChangeText={(val) => setFname(val)}/>
+<TextInput placeholder="Enter your lastname" onChangeText={(val) => setLname(val)}/>
+<TextInput placeholder="Enter your date of birth" onChangeText={(val) => setDob(val)}/>
+
     </View>
   )
 };
